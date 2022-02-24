0fd4adefdb52b7502b5c7da616613e3a7660da2a
ValentinAlmeida <valentinfalmeida@hotmail.com>
2022-02-24 12:07:24 -0300

    iniciando o layout do sistema

--- application/controllers/Sistema.php
+++ application/controllers/Sistema.php
@@ -1,1 +1,33 @@
 <?php
+defined('BASEPATH') or exit('Ação não permitida');
+
+class Sistema extends CI_Controller{
+
+    /*public function __construct(){
+        parent::__construct();
+
+        if (!$this->ion_auth->logged_in())
+        {
+            redirect('login');
+        }
+
+    }*/
+
+    public function index(){
+
+        $data = array(
+
+            'titulo' => 'Editar informações do sistema',
+            'sub_titulo' => 'Editando as informações do sistema',
+            'icone_view' => 'ik ik-settings',
+            'sistema' => $this->core_model->get_by_id('sistema', array('sistema_id' => 1)),
+
+        );
+
+            $this->load->view('layout/header', $data);
+            $this->load->view('sistema/index');
+            $this->load->view('layout/footer');
+
+}
+
+}
--- application/views/layout/sidebar.php
+++ application/views/layout/sidebar.php
@@ -1,6 +1,6 @@
 <div class="app-sidebar colored">
     <div class="sidebar-header">
-        <a class="header-brand" href="index.html">
+        <a class="header-brand" href="<?php echo base_url('/') ?>">
             <div class="logo-img">
 
                 <img src="<?php echo base_url('public/src/img/brand-white.svg')?>" class="header-brand-img" alt="lavalite">
@@ -16,18 +16,18 @@
             <nav id="main-menu-navigation" class="navigation-main">
                 <div class="nav-lavel">Navegação</div>
                 <div class="nav-item active">
-                    <a data-toggle="tooltip" data-placement="right" title="Dashboards" href="index.html"><i class="ik ik-bar-chart-2"></i><span>Dashboard</span></a>
+                    <a data-toggle="tooltip" data-placement="right" title="Dashboards" href="<?php echo base_url('/') ?>"><i class="ik ik-bar-chart-2"></i><span>Dashboard</span></a>
                 </div>
                 <div class="nav-item">
-                    <a data-toggle="tooltip" data-placement="right" title="Navegação" href="<?php echo base_url('public/pages/navbar.html')?>"><i class="ik ik-menu"></i><span>Navegação</span> <span class="badge badge-success">New</span></a>
+                    <a data-toggle="tooltip" data-placement="right" title="Navegação" href="<?php echo base_url('/')?>"><i class="ik ik-menu"></i><span>Navegação</span> <span class="badge badge-success">New</span></a>
                 </div>
                 <div class="nav-item has-sub">
                     <a data-toggle="tooltip" data-placement="right" title="Widgets" href="javascript:void(0)"><i class="ik ik-layers"></i><span>Widgets</span> <span class="badge badge-danger">150+</span></a>
                     <div class="submenu-content">
-                        <a href="<?php echo base_url('public/pages/widgets.html')?>" class="menu-item">Basic</a>
-                        <a href="<?php echo base_url('public/pages/widget-statistic.html')?>" class="menu-item">Statistic</a>
-                        <a href="<?php echo base_url('public/pages/widget-data.html')?>" class="menu-item">Data</a>
-                        <a href="<?php echo base_url('public/pages/widget-chart.html')?>" class="menu-item">Chart Widget</a>
+                        <a href="<?php echo base_url('/')?>" class="menu-item">Basic</a>
+                        <a href="<?php echo base_url('/')?>" class="menu-item">Statistic</a>
+                        <a href="<?php echo base_url('/')?>" class="menu-item">Data</a>
+                        <a href="<?php echo base_url('/')?>" class="menu-item">Chart Widget</a>
                     </div>
                 </div>
                 <div class="nav-lavel">Administração</div>
